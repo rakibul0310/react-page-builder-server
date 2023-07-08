e16e09119c1a6177fe69140c77ebf70e3b12f9d0
rakibul2180@gmail.com <rakibul2180@gmail.com>
2023-07-08 15:18:53 +0600

    fix routes

--- routes/UI_Routes/index.js
+++ routes/UI_Routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { home } = require("../../controllers/UI_Controller");
+const { home, editor } = require("../../controllers/UI_Controller");
 
 const router = express.Router();
 
 router.get("/home", home);
-router.get("/editor/:id", findAllProject);
+router.get("/editor/:id", editor);
 router.all("*", (req, res) => {
   res.render("404");
 });
